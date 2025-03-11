8bd7a3bd29d52ef02ee465ecbbb602d955d631af
test <swrna.mishra@gmail.com>
2025-03-11 13:54:29 -0600

    Modified Files

--- test/features/step-definations/Alerts.ts
+++ test/features/step-definations/Alerts.ts
@@ -6,12 +6,12 @@ import { Given,When,Then }  from "@wdio/cucumber-framework";
 //     await browser.pause(3000)
 // })
 
-// Then (/^Handle the alert$/, async function () {
-//     await browser.pause(2000)
-//     if (await browser.isAlertOpen())
-//     {
-//         await browser.acceptAlert() //to click on OK button on alert
-//         await browser.pause(5000)
-//     }
-//     await browser.pause(5000)
-// })
+Then (/^Handle the alert$/, async function () {
+    await browser.pause(2000)
+    if (await browser.isAlertOpen())
+    {
+        await browser.acceptAlert() //to click on OK button on alert
+        await browser.pause(5000)
+    }
+    await browser.pause(5000)
+})
